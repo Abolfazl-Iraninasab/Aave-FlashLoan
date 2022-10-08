9b7720c0ca0822630e7c8a835ee99be190316366
Abolfazl <ab.iraninasab.crypto1400@gmail.com>
2022-10-08 07:51:29 +0330

    flashLoan test bugs fixed

--- test/flashLoan.js
+++ test/flashLoan.js
@@ -2,15 +2,15 @@ const BN = require("bn.js")
 const { sendEther, pow } = require("./util")
 
 const IERC20 = artifacts.require("IERC20")
-const testFlashLoan = artifacts.require("testFlashLoan")
+const flashLoan = artifacts.require("flashLoan")
 
-contract("testFlashLoan", (accounts) => {
-    const WHALE = 0x89dF4F398563bF6A64a4D24dFE4be00b20b563Ae            // for both USDT & USDC
-    const TOKEN_BORROW1 = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48    // USDC
-    const TOKEN_BORROW2 = 0xdAC17F958D2ee523a2206206994597C13D831ec7    // USDT
+contract("flashLoan", (accounts) => {
+    const WHALE = "0x89dF4F398563bF6A64a4D24dFE4be00b20b563Ae"            // for both USDT & USDC
+    const TOKEN_BORROW1 = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"    // USDC
+    const TOKEN_BORROW2 = "0xdAC17F958D2ee523a2206206994597C13D831ec7"    // USDT
     // const TOKEN_BORROW = [TOKEN_BORROW1 , TOKEN_BORROW2 ]
     const DECIMALS = 6
-    const FUND_AMOUNT = pow(10, DECIMALS).mul(new BN(150))
+    const FUND_AMOUNT = pow(10, DECIMALS).mul(new BN(200))
     const BORROW_AMOUNT = pow(10, DECIMALS).mul(new BN(100))
 
     const ADDRESS_PROVIDER = "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5"
@@ -19,9 +19,9 @@ contract("testFlashLoan", (accounts) => {
     let token
     before(async () => {
         token1 = await IERC20.at(TOKEN_BORROW1)
-        token2 = await IERC20.at(TOKEN_BORROW1)
+        token2 = await IERC20.at(TOKEN_BORROW2)
 
-        FlashLoan = await testFlashLoan.new(ADDRESS_PROVIDER)   // ADDRESS_PROVIDER is the input of constructor
+        FlashLoan = await flashLoan.new(ADDRESS_PROVIDER)   // ADDRESS_PROVIDER is the input of constructor
 
         await sendEther(web3, accounts[0], WHALE, 1)
 
@@ -37,7 +37,7 @@ contract("testFlashLoan", (accounts) => {
     })
 
     it("do flash loan", async () => {
-        const tx = await FlashLoan.doFlashLoan([TOKEN_BORROW1, TOKEN_BORROW2], [BORROW_AMOUNT, BORROW_AMOUNT], {
+        const tx = await FlashLoan.doFlashLoan(TOKEN_BORROW1, TOKEN_BORROW2, BORROW_AMOUNT, BORROW_AMOUNT, {
             from: WHALE,
         })
         for (const log of tx.logs) {
